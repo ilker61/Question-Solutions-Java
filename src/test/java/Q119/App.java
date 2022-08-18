a30d24209c538c3fa006f89df5c5bf698e035f15
ilker61 <jakyuer@gmail.com>
2022-08-18 19:44:05 +0200

    18.08.22

---
+++ src/test/java/Q119/App.java
@@ -0,0 +1,19 @@
+package Q119;
+
+public class App {
+    String greet="Welcome";
+    public App(){
+        String greet="Hello";
+    }
+    public void setGreet(){
+        String greet="Good Day";
+    }
+
+    public static void main(String[] args) {
+
+        App t=new App();
+        String greet="Good Luck!";
+        System.out.println(t.greet);
+
+    }
+}
---
+++ src/test/java/Q122/Y.java
@@ -0,0 +1,9 @@
+package Q122;
+
+public class Y {
+    public static void main(String[] args) {
+        short s1= 200;
+        Integer s2=400;
+        //String s3=(String) (s1+s2);
+     }
+}
