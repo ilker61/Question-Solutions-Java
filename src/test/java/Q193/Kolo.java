2eaf73042d3fccaa8d1525958871df64b7f64885
ilker61 <jakyuer@gmail.com>
2022-08-22 13:35:48 +0200

    22.08.22

---
+++ src/test/java/Q193/Kolo.java
@@ -0,0 +1,13 @@
+package Q193;
+
+public class Kolo {
+    public static void main(String[] args) {
+        int sum=0;
+        for (int xVal = 1; xVal <=5 ; xVal++) {
+            sum=sum+xVal;
+
+        }
+       //  System.out.println("The sum of " + xVal + " numbers is: " + sum);
+
+    }
+}
---
+++ src/test/java/Q199/Tern.java
@@ -0,0 +1,11 @@
+package Q199;
+
+public class Tern {
+    public static void main(String[] args) {
+        String stuff="TV";
+        String res=null;
+
+        res=stuff.equals("TV")?"Walter":stuff.equals("Movie")?"White":"No Result";
+
+    }
+}
