57d4f2ab5d5eabc5fd4b638d667d441fecb00b4d
ilker61 <jakyuer@gmail.com>
2022-08-23 14:08:01 +0200

    23.08.22 18. soru

--- src/test/java/Q19/Test.java
+++ src/test/java/Q19/Test.java
@@ -2,20 +2,12 @@ package Q19;
 
 public class Test {
 
-    static int a;
-    int b;
+
+    final static void ddd(){
+        System.out.println("Bir Classta final static methods olabilier");
+    }
 
     public static void main(String[] args) {
-        System.out.println(a);  // static veriableler kullanilmadan evvel initialize edilmek zorunda degil
-        //System.out.println(b);  ==> instance veriableler kullanilmadan evvel initialize edilmek zorunda
-
-
-    }
-    private Test(){
-
-    }
-
-    private Test(int x){
-        // Bir class ta overload edilmis private constructor olabilir
+        ddd();
     }
 }
