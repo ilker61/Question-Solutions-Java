e53ec5be372bdd791a5384e6a79eddd395d9f601
ilker61 <jakyuer@gmail.com>
2022-08-24 17:54:00 +0200

    24.08.22 .

---
+++ src/test/java/Q71/C1.java
@@ -0,0 +1,23 @@
+package Q71;
+/*
+public class C1 extends C2 implements I{
+    public void displayI(){
+        System.out.println("C1");
+    }
+
+
+
+    public static void main(String[] args) {
+        C2 obj1=new C1();
+        I obj2=new C1;
+
+        C2 s=obj2;
+        I t=obj1;
+
+        t.displayI();
+        s.displayC2();
+    }
+}
+################### NOT: Bir classin iki parenti olmaz. c1 c2 ye extend olmus ayni zamanda I ya implement. Bu olmaz bro
+
+ */
---
+++ src/test/java/Q71/I.java
@@ -0,0 +1,5 @@
+package Q71;
+
+public interface I {
+    public void display();
+}
