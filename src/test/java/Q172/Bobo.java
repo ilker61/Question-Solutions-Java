f0b3aeef5c1a5797036c63e7808c78978162b496
ilker61 <jakyuer@gmail.com>
2022-08-26 15:24:46 +0200

    26.08.22 .

---
+++ src/test/java/Q172/Bobo.java
@@ -0,0 +1,12 @@
+package Q172;
+
+public class Bobo {
+    public static void main(String[] args) {
+        int num=5;
+        do {
+            System.out.println(num-- + " ");
+        }while (num==0);
+    }
+    //// El cevab=  5
+
+}
