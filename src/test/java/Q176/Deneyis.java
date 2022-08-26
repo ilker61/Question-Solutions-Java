b980a10b409ea3e96b0cc0481776b37c7f104ac5
ilker61 <jakyuer@gmail.com>
2022-08-26 15:58:44 +0200

    26.08.22 .

---
+++ src/test/java/Q176/Deneyis.java
@@ -0,0 +1,7 @@
+package Q176;
+
+public abstract class Deneyis {
+    public abstract void process();
+
+    // abstract classta abstract method yazilacagisa mutlakatuttabii abstract ifadesi yaz eylenmelidir.
+}
